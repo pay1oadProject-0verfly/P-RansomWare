763677b4dce363fbe188d306705bf5b276399620
smpark0213 <smpark0213@naver.com>
2020-12-02 20:30:42 +0900

    파일인지 아닌지에 대한 def를 넣었습니다.

--- ELG.py
+++ ELG.py
@@ -1,24 +1,46 @@
 import os
 
-arr = []
+arr = ['.doc', '.docx', '.hwp', '.pptx', '.ppt', '.xls', '.pdf', '.ai', '.psd', '.tx', '.bmp', '.gif', '.png', '.jpg', '.jpeg', '.raw', '.tiff', '.was', '.wma', '.mp3', '.mp4', '.mkv', '.avi', '.flv', '.mov', '.7z', '.aip', 'alz', '.egg', '.zip', 'py', 'c', 'cpp', 'java', 'class', 'html', '.ini', 'lnk']
 
-while True:
-    main_file = open("ELG.txt", "r", encoding='UTF8')
-    lines = main_file.readlines()
-    if not arr: break
-    arr.append(lines)
-    main_file.close()
+forChange = []
+noChange = []
+
+#while True:
+
+a = 'C:\\Users\\Ebaman\\Desktop\\
+a += 'ELG.txt'
+
+i = 0
+j = 0
+
+print(os.path.isfile(a))
+
+
+path_dir = 'C:\\Users\\Ebaman\\Desktop'
+ 
+file_list = os.listdir(path_dir)
+
+def isFile(): #폴더인지 아닌지를 확인해서 폴더면 noChange에 넣고 확장자가있는 파일이면 forChange에 저장.
+    for i in range(0, len(file_list)): 
+        for j in range(0, len(arr)):
+            if file_list[i].find(arr[j]) >= 0:
+                forChange.append(file_list[i])
+                
+    file_list_set = set(file_list)
+    forChange_set = set(forChange)
+    noChange_set = set(noChange)
+
+    noChange_set = file_list_set - forChange_set
+    noChange = list(noChange_set)
     
-#print(lines)
-print(arr)
+
+print(forChange)
+print("-----------------------------------------------------------------------------------")
+print(noChange)
+
+
+#print(len(file_list))
 '''
-file_route = input("경로를 입력하세요: ")
-print(os.listdir(file_route))   # 해당 경로에 있는 파일들의 이름이 리스트의 형태로 출력됨
-file = input("파일 이름을 입력하세요(확장자까지 모두 입력): ")
-file_name, file_Extension = os.path.splitext(file)
-
-
-for item in arr:
-    filename, fileExtension = os.path.splitext(item)
-    print('파일명 : ' + filename + '  확장자명 : ' + fileExtension)
-    '''
+    for file in os.listdir("C:\\Users\\Ebaman\\Desktop"):
+        if file.endswith(".txt"):
+            print(os.path.join("C:\\Users\\Ebaman\\Desktop", file))       '''
